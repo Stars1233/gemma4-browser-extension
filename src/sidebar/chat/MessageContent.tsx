7eb2bbff58972e2dfd80a90c63c4f49132289d82
Nico Martin <mail@nico.dev>
2026-02-18 11:04:00 +0100

    wraped content

--- src/sidebar/chat/MessageContent.tsx
+++ src/sidebar/chat/MessageContent.tsx
@@ -18,7 +18,7 @@ export default function MessageContent({
       {tools && tools.length > 0 && <MessageToolCall tools={tools} />}
       {Boolean(content) ? (
         <div
-          className="prose prose-invert prose-li:text-sm prose-headings:text-sm prose-p:text-sm prose-headings:font-semibold prose-p:my-2 prose-ul:my-2 prose-li:my-0 prose-hr:my-4 max-w-none"
+          className="prose prose-invert prose-li:text-sm prose-headings:text-sm prose-p:text-sm prose-headings:font-semibold prose-p:my-2 prose-ul:my-2 prose-li:my-0 prose-hr:my-4 max-w-none break-words overflow-wrap-anywhere"
           dangerouslySetInnerHTML={{
             __html: converter.makeHtml(content),
           }}
